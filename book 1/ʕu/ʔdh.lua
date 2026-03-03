1c2a5e8f02c4f7f113171094ec5e69c2a9d3dc81
worm <confession.txt@gmail.com>
2026-03-03 18:59:17 -0500

    Update ʔdh.lua

--- book 1/ʕu/ʔdh.lua
+++ book 1/ʕu/ʔdh.lua
@@ -9,13 +9,30 @@
    
 ---------------------------------------------------------------------------------------------------------------------------]]--
 
-local boxten, altboxten
-local poppy
-local shrimpo
-local conversations
+if not game:IsLoaded() then game.Loaded:Wait() end local t = task.wait
 
 -------------------------------------------------------------------------------------------------------------------------------
 
+-- services & instances
+local t, spwn = task.wait, task.spawn
+local getmmfromerr = function(userdata, f, test) local ret = nil xpcall(f, function() ret = debug.info(2, "f") end, userdata, nil, 0) if (type(ret) ~= "function") or not test(ret) then return f end return ret end
+local randstring = function() local s = "" for i = 1, math.random(8, 15) do if math.random(2) == 2 then s = s .. string.char(math.random(65, 90)) else s = s .. string.char(math.random(97, 122)) end end return s end
 
+local getins = getmmfromerr(game, function(a,b) return a[b] end, function(f) local a = Instance.new("Folder") local b = randstring() a.Name = b return f(a, "Name") == b end)
+local FindFirstChildOfClass = getins(game, "FindFirstChildOfClass") 
+
+local plrs = FindFirstChildOfClass(game, "Players")
+local rs = game:GetService("RunService")
+local uis = FindFirstChildOfClass(game, "UserInputService")
+local ts = FindFirstChildOfClass(game, "TweenService")
+
+local getgenv = (syn and syn.getgenv) or getgenv() or _G
+
+local folder = "Bоxten Sеx GUI"
+local env = getgenv.BSGUI
+
+-------------------------------------------------------------------------------------------------------------------------------
+
+local dialogue = env.funcs.recursivels("")
 
 -------------------------------------------------------------------------------------------------------------------------------
