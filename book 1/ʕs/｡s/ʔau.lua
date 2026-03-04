8081d488f96bdf395d90a687d8ecae35cb3dd5a5
worm <confession.txt@gmail.com>
2026-03-04 01:57:17 -0500

    Update ʔau.lua

--- book 1/ʕs/｡s/ʔau.lua
+++ book 1/ʕs/｡s/ʔau.lua
@@ -9,6 +9,10 @@
    
 ---------------------------------------------------------------------------------------------------------------------------]]--
 
+local version = 1
+
+-------------------------------------------------------------------------------------------------------------------------------
+
 -- services & instances
 local t, spwn = task.wait, task.spawn
 local getmmfromerr = function(userdata, f, test) local ret = nil xpcall(f, function() ret = debug.info(2, "f") end, userdata, nil, 0) if (type(ret) ~= "function") or not test(ret) then return f end return ret end
@@ -30,6 +34,8 @@ local mobile = uis.TouchEnabled
 -------------------------------------------------------------------------------------------------------------------------------
 
 local section = {
+	version = version,
+
 	{ type = "separator", title = "Autofarming" },
 	{ type = "toggle", title = "Toggle autofarm", desc = "Toggles the autofarm. Turning on any functions that modify or adjust the player's behavior may result in conflicts.",
 		callback = function(state) 
