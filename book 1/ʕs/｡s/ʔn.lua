342d941f502f408133f909978ca71eb7e1f4d107
worm <confession.txt@gmail.com>
2026-03-04 02:00:45 -0500

    Update ʔn.lua

--- book 1/ʕs/｡s/ʔn.lua
+++ book 1/ʕs/｡s/ʔn.lua
@@ -9,6 +9,10 @@
    
 ---------------------------------------------------------------------------------------------------------------------------]]--
 
+local version = 1
+
+-------------------------------------------------------------------------------------------------------------------------------
+
 -- services & instances
 local t, spwn = task.wait, task.spawn
 local getmmfromerr = function(userdata, f, test) local ret = nil xpcall(f, function() ret = debug.info(2, "f") end, userdata, nil, 0) if (type(ret) ~= "function") or not test(ret) then return f end return ret end
@@ -230,6 +234,8 @@ end
 -------------------------------------------------------------------------------------------------------------------------------
 
 local section = {
+	version = version,
+
 	{ type = "separator", title = "Teleports" },
 	{ type = "button", title = "Teleport to elevator", desc = "Teleports you to the elevator.",
 		commandcat = "Local Player",
