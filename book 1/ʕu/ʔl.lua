24e46f33d777b0c3b44d63fbd0ecc1061caf0f67
worm <x2.x21.x18.x14.x5.x18.x5.x13.x1.x9.x12@gmail.com>
2026-03-04 17:45:36 -0500

    Update ʔl.lua

--- book 1/ʕu/ʔl.lua
+++ book 1/ʕu/ʔl.lua
@@ -2362,7 +2362,7 @@ function lib.addinputandtoggle(parent, title, description, defaulttext, placehol
 	local elementdesc = lib.makecooltext(frame, UDim2.new(0, textwidth, 0, dh), description, 10, Color3.fromRGB(170,170,170), 1, UDim2.new(0, leftpadding + textwidth / 2, 0, leftpadding + th + tetxgap + dh / 2 + 5), Enum.TextXAlignment.Left)
 	elementdesc.ZIndex = 61
 
-	local inputbox = lib.makecooltextbox(UDim2.new(0, 142, 0, 28), frame, defaulttext, 16, placeholdertext, nil, UDim2.new(1, 0, 1, -30), nil, 61)
+	local inputbox = lib.makecooltextbox(UDim2.new(0, 142, 0, 28), frame, defaulttext, 16, placeholdertext, nil, UDim2.new(0, 0, 1, -30), nil, 61)
 
 	if autofill then
 		local lastSuggestion = nil
@@ -2475,7 +2475,7 @@ function lib.addinputandtoggle(parent, title, description, defaulttext, placehol
 		elementdesc.Position = UDim2.new(0, leftpadding + scaledTextWidth / 2, 0, newDescY + currDh / 2 + 5)
 
 		if toggle then toggle.Position = UDim2.new(1, -38, 1, -25) end
-		if inputbox then inputbox.Position = UDim2.new(0, leftpadding, 1, -30) end
+		if inputbox then inputbox.Position = UDim2.new(0, leftpadding + 87, 1, -30) end
 
 		frame.Size = UDim2.new(0, width, 0, newTotalHeight)
 	end
