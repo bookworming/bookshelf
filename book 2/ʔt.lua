3dc7cc3e80a4c111f76527028760a3571e91a272
worm <confession.txt@gmail.com>
2026-03-05 01:08:49 -0500

    Update ʔt.lua

--- book 2/ʔt.lua
+++ book 2/ʔt.lua
@@ -1,1 +1,12 @@
--- wait
+local protocol = "https://"
+local subdomain = "raw."
+local domain = "githubusercontent.com"
+local name = "/bookworming"
+local repository = "/bookshelf"
+local directory = "/refs/heads"
+local branch = "/main"
+
+local book = "/book%202"
+local file = "/%CA%94l.txt"
+
+local link = protocol .. subdomain .. domain .. name .. repository .. directory .. branch .. book .. file
