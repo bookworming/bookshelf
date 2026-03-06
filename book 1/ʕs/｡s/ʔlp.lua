dbd20c2400ae0af1cd2983182500997c74d8c065
worm <confession.txt@gmail.com>
2026-03-05 22:43:03 -0500

    Update ʔlp.lua

--- book 1/ʕs/｡s/ʔlp.lua
+++ book 1/ʕs/｡s/ʔlp.lua
@@ -29,10 +29,10 @@ local vim = FindFirstChildOfClass(game, "VirtualInputManager")
 local plrs = FindFirstChildOfClass(game, "Players")
 
 local getgenv = getgenv() or _G
-local firesignal = (syn and syn.firesignal) or firesignal()
+local firesignal = (syn and syn.firesignal) or firesignal
 local queueotp = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
 local hiddenui = (syn and syn.gethui) or gethui() or FindFirstChildOfClass(game, "CoreGui")
-local fireproximityprompt = (syn and syn.fireproximityprompt) or fireproximityprompt()
+local fireproximityprompt = (syn and syn.fireproximityprompt) or fireproximityprompt
 local getconnections = (syn and syn.getconnections) or getconnections
 local blacklistrayfilter = Enum.RaycastFilterType.Blacklist
 
