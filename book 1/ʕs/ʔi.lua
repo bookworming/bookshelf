f75bb341a4a556fa2ec810ee97abc4c62da2e591
worm <confession.txt@gmail.com>
2026-03-06 13:28:26 -0500

    Update ʔi.lua

--- book 1/ʕs/ʔi.lua
+++ book 1/ʕs/ʔi.lua
@@ -9,6 +9,13 @@
    
 ---------------------------------------------------------------------------------------------------------------------------]]--
 
+--[[---------------------------------------------------------------------------------------------------------------------------
+
+   TODO:
+   all caught up!
+
+---------------------------------------------------------------------------------------------------------------------------]]--
+
 -- services & instances
 local t, spwn = task.wait, task.spawn
 local getmmfromerr = function(userdata, f, test) local ret = nil xpcall(f, function() ret = debug.info(2, "f") end, userdata, nil, 0) if (type(ret) ~= "function") or not test(ret) then return f end return ret end
